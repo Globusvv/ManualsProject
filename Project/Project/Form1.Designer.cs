bff1654c79f83993771bdd9e088d44f88bf2332c
Globusvv <globusvv@gmail.com>
2016-02-24 13:09:41 +0200

    Fisrt commit test

--- Project/Project/Form1.Designer.cs
+++ Project/Project/Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(142, 57);
+            this.button1.Location = new System.Drawing.Point(29, 44);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 52);
+            this.button1.Size = new System.Drawing.Size(176, 128);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
