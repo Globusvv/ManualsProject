3a16f786cfd5bdd0d9cf4daae6e622de7af5a150
SomeShapa <pavel.shipicyn@gmail.com>
2016-02-24 14:15:20 +0300

    TEst commit

--- Project/Project/Form1.Designer.cs
+++ Project/Project/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(29, 44);
+            this.button1.Location = new System.Drawing.Point(96, 112);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(176, 128);
             this.button1.TabIndex = 0;
